bd3a3d4f5f1ade652a4c1c07fc3c48dce7178c5d
Ned Collyer <ned.collyer@gmail.com>
2023-11-04 13:28:58 +1100

    👌 IMPROVE: lint

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$components: 'src/components'
-		}
-	}
+			$components: 'src/components',
+		},
+	},
 };
 
 export default config;
